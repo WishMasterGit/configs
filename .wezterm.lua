4516a775c35dc2c85c70b32376b82ecc82a4fe0a
wishmaster <wishmasterts@gmail.com>
2026-03-19 20:22:26 -0400

    udpate background

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,4 +6,13 @@ config.font = wezterm.font("Cascadia Code")
 config.hide_tab_bar_if_only_one_tab = true
 config.enable_kitty_graphics = true
 config.default_prog = { "/bin/zsh", "-l", "-c", "/opt/homebrew/bin/tmux new-session -A -s main" }
+config.native_macos_fullscreen_mode = true
+config.window_background_image = "/Users/wmts/background.jpg"
+config.window_background_image_hsb = {
+	brightness = 0.04,
+	hue = 1.0,
+	saturation = 1.0,
+}
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 return config
